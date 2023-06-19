2ca2cca42f57dc59d70bd3906163ec19fa0b8770
Andrey Baranov <tread_lightly@list.ru>
2023-06-19 13:23:11 +1000

    5.6. Простые числа [#156314 #423848]

--- src/main/java/ru/job4j/loop/PrimeNumber.java
+++ src/main/java/ru/job4j/loop/PrimeNumber.java
@@ -3,31 +3,10 @@ package ru.job4j.loop;
 public class PrimeNumber {
     public static int calc(int finish) {
         int count = 0;
-        /*boolean isTrue = true;
-        for (int i = 2; i <= finish; i++) {
-            for (int j = i - 1; j < finish; j++) {
-                if (i % j == 0) {
-                    isTrue = false;
-                    break;
-                }
-            }
-            if (isTrue) {
+        for (int number = 2; number <= finish; number++) {
+            if (CheckPrimeNumber.check(number)) {
                 count++;
             }
-            isTrue = true;
-        }*/
-        boolean isTrue = finish > 1;
-        for (int i = 2; i <= finish; i++) {
-            for (int j = 2; j < i; j++) {
-                if (i % j == 0) {
-                    isTrue = false;
-                    break;
-                }
-            }
-            if (isTrue) {
-                count++;
-            }
-            isTrue = true;
         }
         return count;
     }
